de2b67b76dc821d557412f67d7b40770f64588a0
MrMz <amobarez00@gmail.com>
2021-10-25 17:48:16 +0330

    apidoc for seller's card number APIs

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-10-23T14:32:48.862Z",
+    "time": "2021-10-25T14:17:03.601Z",
     "url": "https://apidocjs.com",
     "version": "0.28.1"
   }
--- app/routes/seller/v1/financial.js
+++ app/routes/seller/v1/financial.js
@@ -4,12 +4,88 @@ const router = express.Router()
 const { seller: sellerController } = config.path.controllers
 const FinancialController = require(`${sellerController}/v1/FinancialController`)
 
+
+/**
+  * @api {get} /api/seller/v1/financial/card GetCardNumber
+  * @apiVersion 1.0.0
+  * @apiName getCardNumber
+  * @apiDescription get seller's card number
+  * @apiGroup Seller
+  * @apiSuccessExample {json} Success-Response:
+  * {
+  *   success: true,
+  *   message: "card number is found!",
+  *   data: {
+  *     _id: "123456789",
+  *     bankId: "123456789"  
+  *   }
+  * }
+  * @apiErrorExample {json} Error-Response:
+  * {
+  *   success: false,
+  *   message: "card number field is empty"
+  * }
+*/
 router.get('/card', FinancialController.getCard.bind(FinancialController))
 
+/**
+  * @api {post} /api/seller/v1/financial/card AddCardNumber
+  * @apiVersion 1.0.0
+  * @apiName addCardNumber
+  * @apiDescription add seller's card number
+  * @apiGroup Seller
+  * @apiParam  {varchar} cardNum seller card number
+  * @apiSuccessExample {json} Success-Response:
+  * {
+  *   success: true,
+  *   message: "card number added successfully!"
+  * }
+  * @apiErrorExample {json} Error-Response:
+  * {
+  *   success: false,
+  *   message: "card number has been added!"
+  * }
+*/
 router.post('/card', FinancialController.addCard.bind(FinancialController))
 
+
+/**
+  * @api {put} /api/seller/v1/financial/card/:num EditCardNumber
+  * @apiVersion 1.0.0
+  * @apiName editCardNumber
+  * @apiDescription edit seller's card number
+  * @apiGroup Seller
+  * @apiParam  {varchar} num seller card number
+  * @apiSuccessExample {json} Success-Response:
+  * {
+  *   success: true,
+  *   message: "card number edited"
+  * }
+  * @apiErrorExample {json} Error-Response:
+  * {
+  *   success: false,
+  *   message: "card number field is empty!"
+  * }
+*/
 router.put('/card/:num', FinancialController.editCard.bind(FinancialController))
 
+/**
+  * @api {delete} /api/seller/v1/financial/card DeleteCardNumber
+  * @apiVersion 1.0.0
+  * @apiName deleteCardNumber
+  * @apiDescription delete seller's card number
+  * @apiGroup Seller
+  * @apiSuccessExample {json} Success-Response:
+  * {
+  *   success: true,
+  *   message: "card number removed!"
+  * }
+  * @apiErrorExample {json} Error-Response:
+  * {
+  *   success: false,
+  *   message: "card number field is empty!"
+  * }
+*/
 router.delete('/card', FinancialController.removeCard.bind(FinancialController))
 
 module.exports = router
