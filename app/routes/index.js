9026085ec61eb1b57c5d7ec36fef86860230557b
MrMz <amobarez00@gmail.com>
2021-11-17 12:32:34 +0330

    add cors

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const cors = require('cors')
 
 const customer = require('./customer')
 const seller = require('./seller')
 const admin = require('./admin')
+
+const corsOptions = {
+    origin: '*',
+    credentials: true,          //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
+router.use(cors(corsOptions))
 
 router.use('/customer', customer)
 router.use('/seller', seller)
